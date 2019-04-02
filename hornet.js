9431af75e9a0bbe835d77b7fdf834665384c9c3f
Noah MacMurchy <36996119+njmacmurchy96@users.noreply.github.com>
2019-04-02 05:27:16 -0500

    Update hornet.js - added GPLv3 header

--- hornet.js
+++ hornet.js
@@ -8,6 +8,24 @@
 // @run-at      document-end
 // ==/UserScript==
 
+/******************** ******************** ******************** 
+                            Hornet
+                      Noah MacMurchy © 2019 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>. 
+******************** ******************** ******************** */
+
 // Create our window to our URL/source w/ '_blank' to avoid popup blocking or other JS issues
 // TODO: Update width and height based on iframe size
 function createWindow(url) {
